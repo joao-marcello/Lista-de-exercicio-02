11614828b64c447140b7ebe45063ef5240b61bfe
João Marcello <126107851+joao-marcello@users.noreply.github.com>
2026-04-23 19:39:52 -0300

    Update ex 11.cpp

--- ex 11.cpp
+++ ex 11.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     double p1, p2_necessaria;
-
+    cout << "Digite o valor da P1:";
     cin >> p1;
 
-    p2_necessaria = (15 - p1) / 2;
+    p2_necessaria = (15 - p1) / 3;
 
     cout << "Precisa tirar: " << p2_necessaria;
 
